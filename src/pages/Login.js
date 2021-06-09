45d15260f9241e1c4176c2f5a3dca85c84f2211f
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-09 17:07:48 -0300

    commit before change fontfamily

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { userLogin } from '../actions';
 import { checkEmail, checkPassword } from '../utils/login';
 
-import walletSvg from '../assets/wallet.svg'
+import walletSvg from '../assets/wallet.svg';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -25,21 +25,26 @@ const Login = () => {
 
   return (
     <main className="h-screen bg-gray-100 flex flex-col justify-center px-20">
-      <section className="h-4/5 flex shadow-lg"> 
-        <section className="flex flex-col items-center gap-2 w-1/2 bg-white rounded p-6">
+      <section className="h-4/5 flex shadow-lg">
+        <section className="flex flex-col items-center gap-6 w-1/2 bg-white rounded p-6">
           <input
-            className="w-3/5 p-2 text-lg outline-none border-2 border-green-100 rounded-lg"
+            className="w-3/5 py-2 px-4 text-lg outline-none border-2 border-green-200 rounded-xl"
             type="email"
             data-testid="email-input"
-            onChange={ (e) => setEmail(e.target.value) }
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
-            className="w-3/5 p-2 text-lg outline-none border-2 border-green-100 rounded-lg"
+            className="w-3/5 py-2 px-4 text-lg outline-none border-2 border-green-200 rounded-xl"
             type="password"
             data-testid="password-input"
-            onChange={ (e) => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button type="button" disabled={ !checkLogin() } onClick={ addUser }>
+          <button
+            className="w-3/5 p-3 bg-purple-500 rounded-xl text-xl text-white shadow"
+            type="button"
+            disabled={!checkLogin()}
+            onClick={addUser}
+          >
             <Link to="/carteira">Entrar</Link>
           </button>
         </section>
